e045a25bcfaba67ce7234da13b364a36ce2c388e
Kaan Kayali <citrusshooter1980@gmail.com>
2023-05-02 17:37:53 +0200

    eater

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,39 @@ import "./styles.css"
 
 export default function App(){
 
-  return "Hi"
+  const [amount, setAmount] = useState(0)
+
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <h1>layla - Spinwheel</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="number" min="1" className="amount" onChange={e => setAmount(e.target.value)}/>
+          <button className="btn">Add</button>
+        </form> 
+        <ul>
+          {
+            amount.map(amo => {
+              return(
+                <li>
+                  <label htmlFor="eintrag">
+                    Name:
+                  </label>
+                  <input type="text" id="eintrag" className="eintrag"/>
+                </li>
+              )
+            })
+
+          }
+        </ul>
+        
+
+
+      </div>
+    </>
+  )
 }
