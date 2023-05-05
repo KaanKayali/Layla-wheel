842da0cab5d0f741c7bf677f2f26607dbc16fde4
Kaan Kayali <citrusshooter1980@gmail.com>
2023-05-05 10:35:56 +0200

    wheel finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,38 @@
-import React, { useState } from "react";
 import "./styles.css";
-
-const entries = [
-  "Entry 1",
-  "Entry 2",
-  "Entry 3",
-  "Entry 4",
-  "Entry 5",
-  "Entry 6",
-  "Entry 7",
-  "Entry 8",
-  "Entry 9",
-  "Entry 10"
-];
+import WheelComponent from "react-wheel-of-prizes";
 
 export default function App() {
-  const [spinning, setSpinning] = useState(false);
-  const [result, setResult] = useState("");
 
-  const spinWheel = () => {
-    setSpinning(true);
-    const randomIndex = Math.floor(Math.random() * entries.length);
-    setTimeout(() => {
-      setResult(entries[randomIndex]);
-      setSpinning(false);
-    }, 5000);
+  /*Wheel*/
+  const segments = [
+    "casper",
+    "kaan",
+    ""
+  ];
+  const segColors = ["saddlebrown", "darkred", "grey"];
+  const onFinished = (winner) => {
+    console.log(winner);
   };
 
   return (
-    <>
-    <h1>layla - Spinwheel</h1>
     <div className="App">
-      <h1>Spinning Wheel</h1>
-      <div className={`wheel ${spinning ? "spinning" : ""}`}>
-        {entries.map((entry, index) => (
-          <div key={index} className="entry">{entry}</div>
-        ))}
+      <div>
+        {/*Wheel*/}
+        <WheelComponent
+          segments={segments}
+          segColors={segColors}
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="black"
+          contrastColor="lightgrey"
+          buttonText="Layla"
+          isOnlyOnce={false}
+          size={190}
+          upDuration={500}
+          downDuration={600}
+          fontFamily="Times New Roman"
+        />
       </div>
-      <button onClick={spinWheel} disabled={spinning}>Spin</button>
-      {result && <h2>{result}</h2>}
     </div>
-    </>
-    
   );
 }
+
